8ca3894f3ed4a8bb75f1111cedc1653f74c3c6d1
Pieloaf <piercelowe1@gmail.com>
2023-10-24 22:23:28 +0200

    feat: Add basic user registration with Discord

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import { AuthRouter, Session, sessionHandler } from "./sessions";
 import { ChatRouter } from "./chat";
 import { BattleRouter } from "./battle/Battle";
 import { QueueRouter } from "./queue";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
 
@@ -16,7 +19,7 @@ app.use(express.json()); // parse data as json unless otherwise specified
  * [with the exception of steam/overlay/{session_id}/{state}]. This middleware
  * extracts the session key and checks it is a valid session before continuing.
  **/
-app.use((req, res, next) => {
+app.use("/services", (req, res, next) => {
     // steam overlay requests dont end with the session key but the server
     // doesnt seem to do anything with the request anyway so skip check
     if (req.path.startsWith("/services/session/steam/overlay/")) {
@@ -33,33 +36,33 @@ app.use((req, res, next) => {
     // the login route ends with /11 so in that case the user has no session
     // and the extracted "key" will be 11, so in this case the request can continue
     if (!session && session_key !== "11") {
-        res.sendStatus(403);
+        res.status(403);
         return;
     }
 
     // adding the session object to the request object so
     // each module doesn't need to lookup the session again
     (req as any).session = session;
-    next();
+    app._router.handle(req, res, next);
 });
 
-app.use("/services/auth", AuthRouter);
+app.use("/auth", AuthRouter);
 
-app.use("/services/chat", ChatRouter);
+app.use("/chat", ChatRouter);
 
-app.use("/services/vs", QueueRouter);
+app.use("/vs", QueueRouter);
 
-app.use("/services/battle", BattleRouter);
+app.use("/battle", BattleRouter);
 
 // request leaderboard or update server of location
-app.post("/services/game/leaderboards/:session_key", (req, res) => {
+app.post("/game/leaderboards/:session_key", (req, res) => {
     // parse board_ids and tourney from body
     // and lookup database
     res.json(JSON.parse(readFileSync("./data/lboard.json", "utf-8")));
 });
 
 // poll for relevant data
-app.get("/services/game/:session_key", (req, res) => {
+app.get("/game/:session_key", (req, res) => {
     let session: Session = (req as any).session;
     // send buffered data and clear
     if (session.data.length > 0) {
@@ -74,23 +77,37 @@ app.get("/services/game/:session_key", (req, res) => {
  * Random routes that either have temp data or idk what their purpose is
  */
 
+const getTokenFromHeader = (req: express.Request) => {
+    return req.headers.authorization?.match(/Bearer (.*)/)?.[1];
+};
 // get account info
-app.get("/services/account/info/:session_key", (req, res) => {
+app.get("/account/info/:session_key?", (req, res) => {
+    let auth: string | undefined =
+        req.params.session_key || getTokenFromHeader(req);
+
+    if (!auth) {
+        res.status(403);
+        return;
+    }
     // // look up user in database
     // return user data (will require some handlers for packing data)
     // TODO: implement handlers for packing acc data
     res.json(JSON.parse(readFileSync("./data/acc.json", "utf-8")));
 });
 
-app.post("/services/game/location/:session_key", (req, res) => {
+app.post("/game/location/:session_key", (req, res) => {
     // do something here with location info maybe? idk what
+
+    // TODO: start worker for class linked with location eg meadhouse -> worker for roster
     res.send();
 });
 
 // notify server if steam overlay is enabled
-app.post("/services/session/steam/overlay/:session_key/:state", (req, res) => {
+app.post("/session/steam/overlay/:session_key/:state", (req, res) => {
     // idk what the server does with this info
     res.send();
 });
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(3000, () => {
+    console.log("Express server listening on port " + 3000);
+});
